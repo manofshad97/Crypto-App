7339f5a62664fa65664edd3f1bb5dad98b92b298
manofshad97 <shadman559@hotmail.com>
2022-01-28 18:05:50 -0500

    added express server

---
+++ server.js
@@ -0,0 +1,15 @@
+// initialize express
+const express = require("express");
+const app = express();
+// set default port number
+const port = process.env.PORT || 3000;
+// binding our server to a static directory
+app.use(express.static(path.join(__dirname, 'build')));
+// setting up route handler
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/build/index.html");
+});
+// open a connection to listen for, and respond to requests
+app.listen(port, () => {
+  console.log("Listening on port: " + port)
+});
