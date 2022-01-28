48a9f9962c68e53f9ed8c2f4685df16f9aafc78b
manofshad97 <shadman559@hotmail.com>
2022-01-28 18:54:48 -0500

    fixed server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 // set default port number
 const port = process.env.PORT || 3000;
 // binding our server to a static directory
-app.use(express.static("./client/build"));
+app.use(express.static("client/build"));
 // setting up route handler
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/client/build/index.html");
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 // open a connection to listen for, and respond to requests
 app.listen(port, () => {
